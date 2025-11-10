d55135c48eb365b83ca65001d3835c2c6707bbfe
Shay Fleishon <80166370+shay671@users.noreply.github.com>
2025-11-10 15:49:10 +0200

    Refine Pango unrolling helper

--- pango_explain/__init__.py
+++ pango_explain/__init__.py
@@ -1,6 +1,12 @@
 """Pango explain utilities."""
 
-from .pango_alias import load_alias_map, lookup_alias
+from .pango_alias import load_alias_map, lookup_alias, unroll_pango_name
 from .gui import run_gui, unroll_aliance
 
-__all__ = ["load_alias_map", "lookup_alias", "run_gui", "unroll_aliance"]
+__all__ = [
+    "unroll_pango_name",
+    "load_alias_map",
+    "lookup_alias",
+    "run_gui",
+    "unroll_aliance",
+]
