252f1c0879cf36b7ace1579e3c5029805b49158f
Shay Fleishon <80166370+shay671@users.noreply.github.com>
2025-11-10 15:50:32 +0200

    Revert "Rename Pango unroll helper and tighten validation"

--- pango_explain/__init__.py
+++ pango_explain/__init__.py
@@ -1,12 +1,6 @@
 """Pango explain utilities."""
 
-from .pango_alias import load_alias_map, lookup_alias, unroll_pango_name
+from .pango_alias import load_alias_map, lookup_alias
 from .gui import run_gui, unroll_aliance
 
-__all__ = [
-    "unroll_pango_name",
-    "load_alias_map",
-    "lookup_alias",
-    "run_gui",
-    "unroll_aliance",
-]
+__all__ = ["load_alias_map", "lookup_alias", "run_gui", "unroll_aliance"]
